237b645d486e6a21914cdd3c365aa3e810798ae1
Douglas Ialamov <douglas.ialamov@gmail.com>
2022-08-29 13:27:51 -0300

    feat: endpoint/Home Register and Search
      - Created the frontend structure
      - Created the style for each route and the page for each one also.
      - Even though it is not working correctly, I created the header and
      footer components. The footer specially, needs improvement.
      - The Register page is working correctly. I needed to mock the database
      of brands, because getting it from an API was really hard for free. Even
      mercadolivre API had a lot of buracracy that I spent some good hours
      trying to overcame, just to realize that I could show how to work with
      an API with a mock database.
      - The Search page is on construction. I am using a style of one table
      for everything. I think that is going to be interesting.

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Header } from '../../components/Header/Header';
 import { Footer } from '../../components/Footer/Footer';
 import api from '../../utils/api';
@@ -8,31 +8,66 @@ export function Search() {
 
   const [carList, setCarList] = useState([]);
 
-  const handleChange = async (e) => {
-    await api.get('/salescar')
-      .then((response) => {
-        setCarList(response);
-      })
+  const handleGetAll = async () => {
+    const carListFromDataBase = await api({
+      method: 'get',
+      url: '/carsales'
+    });
+    console.log(carListFromDataBase.data)
+    setCarList(carListFromDataBase.data);
   }
 
   return (
     <div>
       <Header />
-      <div class='form__group background'>
-        <input 
+      <div className='form__group background-search'>
+      <input 
           type="text" 
-          class="form__input" 
+          className="form__input" 
           id="name" 
           placeholder="Car Model or Brand" 
           required=""
           onChange={(e) => handleChange(e)}
         />
-      </div>
-      <div>
-        {carList.map((eachObject) => {
-          eachObject.find((value) => Object.values(value))
-          
-        })}
+      <button
+        class='btn-search'
+        onClick={() => console.log('run')}
+      >
+        Search
+      </button>
+      <button
+      class='btn-search'
+        onClick={() => handleGetAll()}
+      >
+        Return All List
+      </button>
+      <table class='container-search-form' >
+        <thead>
+          <th>Brand</th>
+          <th>Model</th>
+          <th>Fabrication Year</th>
+          <th>Color</th>
+          <th>Price in USD</th>
+          <th>Edit</th>
+          <th>Delete</th>
+        </thead>
+        <tbody>
+          {carList.map((eachCar) => {
+            return (
+            <tr 
+            key={eachCar._id}>
+              <td>{eachCar.brand}</td>
+              <td>{eachCar.model}</td>
+              <td>{eachCar.fabrication_year}</td>
+              <td>{eachCar.color}</td>
+              <td>{eachCar.price}</td>
+              <span>Edit</span>
+              <span>Delete</span>
+            </tr>
+            )
+          })}
+        </tbody>
+      </table>
       </div>
       <Footer />
     </div>
